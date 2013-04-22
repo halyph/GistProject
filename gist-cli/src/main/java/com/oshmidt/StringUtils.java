bfe67b5bb378259228c475260d82c938eb95f0fd
oshmidt <dublikator.com@gmail.com>
2013-04-22 17:42:29 +0300

    close #31

--- gist-cli/src/main/java/com/oshmidt/StringUtils.java
+++ gist-cli/src/main/java/com/oshmidt/StringUtils.java
@@ -2,6 +2,9 @@ package com.oshmidt;
 
 public class StringUtils {
 
+	private StringUtils() {
+	}
+	
 	public static String convertToString(String[] args) {
 		StringBuilder full = new StringBuilder();
 		for (String string : args) {
