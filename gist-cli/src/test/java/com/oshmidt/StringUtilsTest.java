506355c66b27d3fb8397c52b63a4355d38d65ebf
oshmidt <dublikator.com@gmail.com>
2013-04-24 16:56:11 +0300

    add new tests

---
+++ gist-cli/src/test/java/com/oshmidt/StringUtilsTest.java
@@ -0,0 +1,16 @@
+package com.oshmidt;
+
+import static org.junit.Assert.*;
+import org.testng.annotations.Test;
+
+
+public class StringUtilsTest {
+
+	@Test
+	public void testConvertToString() {
+		String [] arr = {"a","b","c","d"};
+		assertTrue(StringUtils.convertToString(arr).equals("a b c d "));
+	}
+	
+
+}
