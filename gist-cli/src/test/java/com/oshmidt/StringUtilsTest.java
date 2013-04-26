220f7b64acd007c6b03e8eecb286a7b1be2b7654
oshmidt <dublikator.com@gmail.com>
2013-04-26 11:36:42 +0300

    new test

--- gist-cli/src/test/java/com/oshmidt/StringUtilsTest.java
+++ gist-cli/src/test/java/com/oshmidt/StringUtilsTest.java
@@ -3,14 +3,17 @@ package com.oshmidt;
 import static org.junit.Assert.*;
 import org.testng.annotations.Test;
 
-
 public class StringUtilsTest {
 
 	@Test
 	public void testConvertToString() {
-		String [] arr = {"a","b","c","d"};
+		String[] arr = {"a", "b", "c", "d"};
 		assertTrue(StringUtils.convertToString(arr, " ").equals("a b c d "));
 	}
-	
 
+	@Test(expectedExceptions = java.lang.IllegalAccessException.class)
+	public void testConvertToStringr() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+		Class<?> cls = Class.forName("com.oshmidt.StringUtils");
+		cls.newInstance();
+	}
 }
