7853f818444bf6d564831b14dc8f21cfd169118f
oshmidt <dublikator.com@gmail.com>
2013-04-29 18:07:02 +0300

    new tests

--- gist-cli/src/test/java/com/oshmidt/GistFetcherTest.java
+++ gist-cli/src/test/java/com/oshmidt/GistFetcherTest.java
@@ -1,38 +1,31 @@
 package com.oshmidt;
 
-import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 
 import org.eclipse.egit.github.core.Gist;
-import org.eclipse.egit.github.core.client.GitHubClient;
-import org.eclipse.egit.github.core.client.GitHubRequest;
-import org.eclipse.egit.github.core.client.GitHubResponse;
 import org.eclipse.egit.github.core.service.GistService;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-
-import static org.mockito.Mockito.*;
 
 public class GistFetcherTest {
 
-
-
-    @Mock
+    // @Mock
     private GistService service;
-
 
     @Before
     public void before() throws IOException {
         service = mock(GistService.class);
         Gist g = new Gist();
         g.setDescription("test gist");
-       // service.getClient().setCredentials(user.getLogin(), user.getPassword());
-    
+        // service.getClient().setCredentials(user.getLogin(),
+        // user.getPassword());
         when(service.getGist("f")).thenReturn(g);
-        
+        // GitHubClient gc = new GitHubClient();
+        // when(service.getClient().thenReturn(gc));
+        // service.getClient()g.th;
     }
 
     @Test
@@ -46,15 +39,15 @@ public class GistFetcherTest {
 
         System.out.println(user.getLogin());
         System.out.println(user.getPassword());
+        gf.loadGist("f", user);
         Gist g = gf.loadGist("f", user);
         System.out.println(g.getDescription());
     }
 
     /*
-    public final Gist loadGist(final String gistId, final User user) throws IOException {
-        setClientCredentials(user);
-        return service.getGist(gistId);
-   }
-*/
+     * public final Gist loadGist(final String gistId, final User user) throws
+     * IOException { setClientCredentials(user); return service.getGist(gistId);
+     * }
+     */
 
 }
--- gist-cli/src/test/java/com/oshmidt/GistManagerTest.java
+++ gist-cli/src/test/java/com/oshmidt/GistManagerTest.java
@@ -1,14 +1,50 @@
 package com.oshmidt;
 
 import static org.junit.Assert.*;
-import org.testng.annotations.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.io.IOException;
+
+import org.junit.Before;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class GistManagerTest {
 
- /*   @Test
+    private GistManager gm;
+    private User user;
+
+    @BeforeMethod
+    public void before() throws IOException {
+        System.out.println("before block");
+        gm = new GistManager();
+        user = new User();
+        
+
+      
+    }
+
+    @Test()
     public void testInitUser() {
-        GistManager gm = new GistManager();
-        gm.initUser("", "");
-    }*/
+        String username = "user";
+        String password = "password";
+        gm.initUser(username, password);
+        assertEquals(username, gm.getUser().getLogin());
+        assertEquals(password, gm.getUser().getPassword());
+    }
+
+    @Test
+    public void testImportUser() {
+        user = mock(User.class);
+        when(user.importUser()).thenReturn(true);
+        
+        gm.importUser();
+    }
+
+    @Test
+    public void testWriteLocalGists() {
+        gm.writeLocalGists();
+    }
+
 }
